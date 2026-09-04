e425ebfc0f9a96b4bb4c4dad785f6ab20a29cc0e
Ishika Chhajed <ishikachhajed68@gmail.com>
2026-09-04 20:01:31 +0530

    Move generated Prisma client outside of src to prevent tsc exclusion

--- apps/server/src/db/seed.ts
+++ apps/server/src/db/seed.ts
@@ -1,4 +1,4 @@
-import { PrismaClient } from '../generated/client/index.js';
+import { PrismaClient } from '../../prisma/generated/client/index.js';
 import fs from 'fs';
 import path from 'path';
 
